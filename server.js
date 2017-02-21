351a6fe39b4e524fa658da4273cfb01cd1dea0cc
Matt OTousa <mlotousa@gmail.com>
2017-02-21 18:11:26 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+'use strict';
+let express = require('express')
+  , app = express()
+  ;
+
+
+app.get('/', function(req, res) {
+  res.send('Hello World');
+});
+
+app.listen(8080, function() {
+  console.log('example app listening on port 8080');
+});
