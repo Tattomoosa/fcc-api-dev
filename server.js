9ac32c092ec1a64f405c02906b9e1f11549186ca
Matt OTousa <mlotousa@gmail.com>
2017-02-26 00:45:41 +0000

    done

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
 'use strict';
 let express = require('express')
   , app = express()
+  , strftime = require('strftime')
   ;
 
-
-app.get('/', function(req, res) {
-  res.send('Hello World');
-});
+app.get('/:date', function(req, res) {
+  //let timeStr = req.params.date;
+  let date = req.params.date
+  date = (function() {
+    let parseDate = new Date(date)
+    if (parseDate == 'Invalid Date') {
+      parseDate = new Date(date*1000)
+      if (parseDate == 'Invalid Date') {
+        return null
+      }
+    }
+    return parseDate
+  })()
+  let stringTime = null, unixTime = null
+  if (date !== null) {
+    stringTime = strftime('%B %d, %Y', date);
+    unixTime = Math.floor(date.getTime() / 1000)
+  }
+  res.send({
+      natural: stringTime
+    , unix: unixTime
+    })
+})
 
 app.listen(8080, function() {
-  console.log('example app listening on port 8080');
-});
+  console.log('example app listening on port 8080')
+})
